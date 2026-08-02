4396180beabe8a657c1f1e8af69ec284b5f14cb7
Pere <pereplus@gmail.com>
2026-08-02 16:00:23 +0200

    Update streamlit_app.py
    Correct API call URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")    
 
 # New section to display smoothiefroot nutrition information:
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
