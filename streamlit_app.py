904f0d713b3c82d69f8045cbb978817090a852f7
Pere <pereplus@gmail.com>
2026-08-02 16:26:50 +0200

    Update streamlit_app.py
    Move JSON request and Dataframe creation to for fruit_chose bucle.

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
 
@@ -45,8 +47,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")    
 
 # New section to display smoothiefroot nutrition information:
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response) # 200 if all run ok
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+#st.text(smoothiefroot_response) # 200 if all run ok
 #st.text(smoothiefroot_response.json())
 # Add JSON data into a dataframe:
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+#sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
